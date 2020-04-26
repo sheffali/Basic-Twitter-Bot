0e79d827441019bc0a1349208c146f8e81d66140
Sheff02 <sheffalisuri@gmail.com>
2020-04-26 23:24:35 +0530

    updation done

--- bot.js
+++ bot.js
@@ -1,30 +1,17 @@
-console.log('hello bby')
-
 var Twit=require('twit');
-
 var config = require('./config');
-
 var T = new Twit(config);
 
-
 tweetIt();
-setInterval(tweetIt,1000*10);
 
+setInterval(tweetIt,1000*10);//posting tweet after given time interval
 function tweetIt()
-{
-var r=Math.floor(Math.random()*100);
-var tweet ={
-status:'test success'+r
-}
+{var r=Math.floor(Math.random()*100);
+var tweet ={status:'test success'+r}////string along with random number
 
+//USING POST REQUEST
 T.post('statuses/update',tweet,tweeted);
 function tweeted(err,data,response)
-{
-if(err)
-   {
-   console.log("erorr bby")
-   }
+{if(err){console.log("error here") }
 else
-{
-  console.log("bhai chal gyaa")
-}}}
+{console.log("twit pasted")}}}
--- bot2.js
+++ bot2.js
@@ -1,26 +1,14 @@
-console.log('hello bby')
 
 var Twit=require('twit');
-
 var config = require('./config');
-
 var T = new Twit(config);
 
-var params={ 
-q: 'coding',//query term
-count:2
-};
+var params={ q: 'coding',count:2};//query term
 
-
-//GET REQUEST TO UNDERSTAND https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets
+//USING GET REQUEST TO UNDERSTAND https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets
 T.get('search/tweets', params, gotData);//keyword and some query term along with function to print data 
 
-
 function gotData (err, data, response) 
-{
-var tweets= data.statuses;
+{var tweets= data.statuses;
 for(var i=0;i<tweets.length;i++)
-{
-console.log(tweets[i].text);
-}
-};
+{console.log(tweets[i].text);}};
